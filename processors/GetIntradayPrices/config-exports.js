9fbab0d22a0ae53c32d1c9eaffc532a4325f37d1
hiralatcal <hiralatcal@yahoo.com>
2020-07-02 01:01:37 -0700

    Initial commit

--- processors/GetIntradayPrices/config-exports.js
+++ processors/GetIntradayPrices/config-exports.js
@@ -1,9 +1,9 @@
 const config = {
-    cognito_pool_id: "eu-west-1_DMhZ3rrXS",
-    cognito_backend_client_id: "pq3rcra4kend08leir0lbth06",
+    cognito_pool_id: "us-west-2_PvG4v8Z1D",
+    cognito_backend_client_id: "5264mvlv0g43h8l5qdlkt1h2ar",
     cognito_backend_access_key: "stockMonitoring/local/backend",
-    appsync_endpoint_url: "https://efrgcmtrnfcn7oshfxf2kvsx4y.appsync-api.eu-west-1.amazonaws.com/graphql",
-    appsync_region: "eu-west-1",
+    appsync_endpoint_url: "https://lymtfcfi4jbdbpu424vndfocza.appsync-api.us-west-2.amazonaws.com/graphql",
+    appsync_region: "us-west-2",
     data_feed_key: "stockMonitoring/local/datafeed",
     feed_interval: "30min"
 }
